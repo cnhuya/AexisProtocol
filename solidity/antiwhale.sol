bad613448569bb5dedeb60722b5d25abfceb7107
huya <potratirl4@seznam.cz>
2025-03-01 23:13:48 +0100

    Update antiwhale.sol

--- solidity/antiwhale.sol
+++ solidity/antiwhale.sol
@@ -2,30 +2,65 @@
 // Compatible with OpenZeppelin Contracts ^4.0.0
 pragma solidity ^0.8.22;
 
+    /**
+    @notice If we call antiwhale() in the constructor, we can add an extra condition that will ensure
+     *      that antiwhale() is only called once per season (i.e., at the start of a new season)
+     
+            [=  ANTI-WHALE-PERIOD FACTOR  =]
+            Maximum Anti-Whale Season Limit: 12 months
+
+            [=  ANTI-WHALE-MAX-DUMP FACTOR  =]
+            Increase Transfer Percentage: 0.5%
+            Starting Maximum Transfer Percentage: 1%
+            Ending Maximum Transfer Percentage: 6% (in 1 tx)
+            fee: 5% for every 1% over the limit
+            ------------------------------------------
+            (e.g. User sends over 3% of total supply in 1 tx,
+            the receiver only receives 90% of the amount, because of 2*5 = 10% burn fee.
+            This would then equal to actually only 2,7% of total supply sent, and 0,3% burned.)
+
+            [=  ANTI-WHALE-SOFT-DUMP FACTOR  =]
+            Decrease Transfer Cooldown: 5 minutes
+            Starting Transfer Cooldown: 55 minutes
+            Ending Transfer Cooldown: 0 minutes
+            Fee: 0.1% for every minute over the limit.
+            ------------------------------------------
+            (e.g. User makes 3 transfers with a 2 minutes delay in between with a transfer cooldown of 10 minutes.
+            This would make a difference of 8 minutes, which would equal to 0,8% burn fee of the sent amount, for each TX.)
+
+            [=  ANTI-SNIPE FACTOR  =]
+            Decrease Snipe Fee per hour: 2%
+            Starting Snipe Fee: 10% 
+            Duration: 5 hours
+    */
+
 
 
 contract AntiWhale{
-    // Declare abc as a state variable
 
-
-    uint256 _initialSupply;
-    uint8 _maxTransferPercentage = 50; // 0.5;
-    uint8 month = 1;
+    // Initial Variables
     uint256 _start;
-    uint8 cooldown = 60;
-    uint16 public snipeFee = 1000; // 10%
-    uint256 previousDiff;
+    uint256 _initialSupply;
+    uint16 maxSeasons = 10;
+    // Anti-Whale-Transfers Variables
+    uint8 _maxTransferPercentage = 50; // 0.5;
+    // Season
+    uint8 _season = 1;
+    // Anti-Snipe Variables
+    uint16 snipeFeeReduction;
+    uint16 snipeFee = 1000; // 10%
+    uint16 visibleSnipeFee;
+    // Anti-Tx-Spam Variables, this is because whales could just split 1 big chunk into smaller transactions
+    uint8 cooldown = 30; // in minutes
+   // uint256 previousDiff;
 
-    constructor(uint256 _initialSupply_) { // Constructor now takes the Aexis contract address
-        _initialSupply = _initialSupply_; // Instantiate Aexis
+    constructor() { 
         _start = block.timestamp;
-    }
-    function initialSupply() public view virtual returns (uint256) {
-        return _initialSupply;
+        visibleSnipeFee = snipeFee;
     }
 
     function viewSeason() public view virtual returns (uint8) {
-        return month;
+        return _season;
     }
 
     function viewTransferPercentage() public view virtual returns (uint8) {
@@ -33,61 +68,65 @@ contract AntiWhale{
         // 50 + (1*50) = 100 (1%)
     }
 
-
-    function viewCooldown() public view virtual returns (uint8) {
-        return cooldown - (viewSeason() * 5);
-        // 60 - (1*5) = 55 minutes
+    function viewSnipeFee() public view virtual returns (uint16) {
+        uint256 _nowInHours = block.timestamp / 1;
+        uint256 _startInHours = _start / 1;
+        uint256 Diff = _nowInHours - _startInHours;
+        uint16 returnValue;
+        if(Diff >= 5){
+            returnValue = 0;    
+        }
+        else{
+            returnValue = snipeFee - (uint16(Diff)*200); 
+        }
+        return returnValue;
     }
 
-    /*
-    @notice If we call antiwhale() in the constructor, we can add an extra condition that will ensure
-            that antiwhale() is only called once per season (i.e., at the start of a new season)
+    function viewCooldown() public view virtual returns (uint8) {
+        uint8 cdr = cooldown - (viewSeason() * 3);
+        uint8 returnValue;
+        if(cooldown <= 0){
+            returnValue = 0;
+        }
+        else {
+            returnValue = cdr;
+        }
+        return returnValue;
+    }
 
-            current set limits are:
-            Maximum Anti-Whale Season Limit: 12 months
-
-            Increase Transfer Percentage: 0.5%
-            Starting Maximum Transfer Percentage: 1%
-            Ending Maximum Transfer Percentage: 6% (in 1 tx)
-            fee: 5% for every 1% over the limit
-
-            (e.g. User sends over 3% of total supply in 1 tx,
-            the receiver only receives 90% of the amount, because of 2*5 = 10% burn fee.
-            This would then equal to actually only 2,7% of total supply sent, and 0,3% burned.)
-
-
-
-            Decrease Transfer Cooldown: 5 minutes
-            Starting Transfer Cooldown: 55 minutes
-            Ending Transfer Cooldown: 0 minutes
-            Fee: 0.1% for every minute over the limit.
-
-            (e.g. User makes 3 transfers with a 2 minutes delay in between with a transfer cooldown of 10 minutes.
-            This would make a difference of 8 minutes, which would equal to 0,8% burn fee of the sent amount, for each TX.)
-
-
-
-            Decrease Snipe Fee per hour: 2%
-            Starting Snipe Fee: 10% 
-            Duration: 5 hours
-
-            
-    */
-
+    /**
+     * @dev Anti-snipe feature, this feature is disabled after few minutes/hours.
+     * Just a preventive settings for either bots or market manipulators such as insiders.
+     * 
+     *
+     * Calculations:
+     *  - Snipe Fee starts at 10%,
+     *  - It gets reduced by 2% every hour, until reaching the end of transfer cooldown (0 minutes).
+     *  - So after 5 hours there is 0 Snipe fee, and this function is completly skipped.
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `value`.
+     */
     function snipe() internal  {
-        uint256 _nowInHours = block.timestamp / 3600;
-        uint256 _startInHours = _start / 3600;
+        uint256 _nowInHours = block.timestamp / 1;
+        uint256 _startInHours = _start / 1;
         uint256 Diff = _nowInHours - _startInHours;
-        uint16 prevSnipeFee = snipeFee;
-        require(prevSnipeFee != snipeFee, "Snipe Fee is the same");
-        snipeFee = snipeFee - (uint16(Diff)*200);
+        if(Diff >= 5){
+            visibleSnipeFee = 0;
+        }
+        else{
+            uint16 prevSnipeFee = snipeFee;
+            visibleSnipeFee = snipeFee - (uint16(Diff)*200); 
+            require(prevSnipeFee != visibleSnipeFee, "Snipe Fee is the same");
+        }
+
         // 1000 - 400 = 600 (6%) after 2 hours
     }
 
     function season() public {
-        require(viewSeason() <= 10, "Maximum Season Limit Reached");
+        require(viewSeason() <= maxSeasons, "Maximum Season Limit Reached");
         uint256 _now = block.timestamp;
-        require((_now / 2592000) > (_start / 2592000));
-        month = month + 1;
+        require((_now / 2592000) > (_start / 2592000), "Time not passed for new season yet");
+        _season = _season + 1;
     }
+
 } 
