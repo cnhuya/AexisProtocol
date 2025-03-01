d8a52b3171d3e1b7312f5055902eca9cdf63694a
huya <potratirl4@seznam.cz>
2025-03-01 23:14:24 +0100

    Update owner.sol

--- solidity/owner.sol
+++ solidity/owner.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Owner{
+
+    address public owner;
+    address public FeeCollector = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        constructor(address _owner)
+    {
+        owner = _owner;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    function setOwner()internal onlyOwner{}
+}
