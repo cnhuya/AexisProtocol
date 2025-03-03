c7a9553d0b5d08ea652d5895907469b8c4ea7a3e
huya <potratirl4@seznam.cz>
2025-03-03 22:21:02 +0100

    Update owner.sol

--- solidity/owner.sol
+++ solidity/owner.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 contract Owner{
 
     address public owner;
+
+    mapping(address => bool) Validator;
+
     address public FeeCollector = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
         constructor(address _owner)
     {
@@ -11,9 +14,22 @@ contract Owner{
     }
     
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
+        require(msg.sender == owner, "Only the owner can perform this action.");
         _;
     }
 
-    function setOwner()internal onlyOwner{}
+    modifier validator() {
+        require(Validator[msg.sender] == true, "Only the validator can perform this action..");
+        _;
+    }
+
+    /*function addValidator(address newValidator) public onlyOwner{
+        require(Validator[newValidator] == false, "Address is already a validator.");
+        Validator[newValidator] = true;
+    }
+
+    function removeValidator(address newValidator) public onlyOwner{
+        require(Validator[newValidator] == true, "Address is not marked as validator.");
+        Validator[newValidator] = false;
+    }*/
 }
