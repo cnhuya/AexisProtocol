e9786ab5127f27d9fc2548cdd8846d775b899740
huya <potratirl4@seznam.cz>
2025-03-05 22:28:35 +0100

    Update token.sol

--- solidity/token.sol
+++ solidity/token.sol
@@ -11,11 +11,9 @@ import {Governance} from "tests/governance.sol";
 contract Aexis is ERC20, ERC20Burnable, ERC20Permit, AntiWhale, Governance {
 
     uint256 private intialSupply = 111 * 10 ** decimals();
-    uint256 public lastTimeClaimed;
-    uint256 public claimed;
 
     constructor()
-        ERC20("Aexis Test", "AXS")
+        ERC20("Aexis Test", "AXS", intialSupply)
         Governance()
         ERC20Permit("Aexis Test")
         AntiWhale()
@@ -26,27 +24,24 @@ contract Aexis is ERC20, ERC20Burnable, ERC20Permit, AntiWhale, Governance {
 
     function transfer(address to, uint256 amount) public override returns (bool) {
         // 100 * 25) / 100 ) / 100
-        uint256 taxxedAmount = ((amount * Fee) / 100) / 100;
+        uint256 taxxedAmount = ((amount * viewFee()) / 100) / 100;
         uint256 _amount = amount - taxxedAmount;
         _transfer(msg.sender, to, _amount);
         _transfer(msg.sender, FeeCollector, taxxedAmount/2);
         burn(taxxedAmount/2);
-        addUserStats(msg.sender, amount/100);
+        addUserStats(msg.sender, amount/1000); // 0.1% of volume
         return true;
     }
 
-    function _Tokenomics() public view returns (uint256 _initialSupply, uint256 _actualSupply, uint256 _burned, uint256 _minted, uint256 _claimed) {
-        return (intialSupply, totalSupply(), burned, minted, claimed);
+    function infoTokenomics() public view returns (uint256 _initialSupply, uint256 _actualSupply, uint256 _burned, uint256 _minted, uint256 _claimed) {
+        return (intialSupply, totalSupply(), burned(), minted(), claimed);
     }
 
     function claimInflation() public onlyOwner {
-        uint256 currentTime = block.timestamp;
-        require(currentTime >= lastTimeClaimed, "Too soon for inflation");
-        uint256 daysPassed = (currentTime - lastTimeClaimed);
-        uint256 inflationAmount = ((intialSupply * (monthlyInflation) * daysPassed) / (100));
-        lastTimeClaimed = currentTime;
-        claimed += inflationAmount;
-        _mint(msg.sender, inflationAmount);
+        require(block.timestamp >= lastTimeClaimed, "Too soon for inflation");
+
+        _mint(msg.sender, getAvaiableToClaim());
+        lastTimeClaimed = block.timestamp;
     }
 
     function check(uint _code) internal onlyOwner{
@@ -57,34 +52,46 @@ contract Aexis is ERC20, ERC20Burnable, ERC20Permit, AntiWhale, Governance {
         require(_now> getProposalEnd(_code), "Previous proposal did not end yet"); 
     }
 
-    function createProposalUint(uint _code, string memory _name_,string memory _description, uint256 _data, uint _period_) public onlyOwner {
+    function proposeUint(uint _code, string memory _name_,string memory _description, uint256 _data, uint _period_) public onlyOwner {
         check(_code);
         createUintProposal(_code, _name_, _description, _data, _period_);
-        vote(proposalCount, balanceOf(msg.sender) + (balanceOf(msg.sender) / proposerVotingPower), true);
+        vote(_code, balanceOf(msg.sender) + (balanceOf(msg.sender) / proposerVotingPower), true);
     }
 
-    function createProposalAddress(uint _code, string memory _name_,string memory _description, address _data, uint _period_) public onlyOwner {
+    function proposeAddress(uint _code, string memory _name_,string memory _description, address _data, uint _period_) public onlyOwner {
         check(_code);
         createAddressProposal(_code, _name_, _description, _data, _period_);
-        vote(proposalCount, balanceOf(msg.sender) + (balanceOf(msg.sender) / proposerVotingPower), true);
+        vote(_code, balanceOf(msg.sender) + ((balanceOf(msg.sender) / proposerVotingPower)), true);
     }
 
     function claimPoints() public {
         UserStats storage stats = users[msg.sender];
         require(getUserPoints(msg.sender) > 0, "Not enough points to claim!");
         uint256 timeHolding = (block.timestamp / 86400) - (stats.holderSince / 86400);
-        // 10000 + 100 * 5 = 10500 total points
         uint256 amount = getUserPoints(msg.sender) + (getUserPoints(msg.sender)/100)*timeHolding;
         stats.points = 0;
-        transferFrom(FeeCollector, msg.sender, amount);
+        _mint(msg.sender, amount);
     }
 
     function Vote(uint _code, bool isYes) public{
         require(getProposalEnd(_code) > block.timestamp, "Proposal already finished");
         require(balanceOf(msg.sender) >= minimumVotingPower, "Invalid balance to vote.");
-        vote(_code, balanceOf(msg.sender),isYes);
         burn(minimumVotingPower);
-        addUserStats(msg.sender, minimumVotingPower);
+        vote(_code, balanceOf(msg.sender),isYes);
+    }
+
+    function getAvaiableToClaim() internal view returns (uint256 avaiable_to_claim) {
+        uint256 daysPassed = (block.timestamp - lastTimeClaimed);
+        uint256 _avaiable_to_claim = (((intialSupply * (monthlyInflation) * daysPassed)) / (100));
+        return(_avaiable_to_claim);
+    }
+
+    function infoClaim() public view returns (uint256 last_time_claimed, uint256 total_claimed, uint256 avaiable_to_claim) {
+        return (getLastTimeClaimed(),getTotalClaimed(), getAvaiableToClaim());
+    }
+
+    function allowMint() public onlyOwner  {
+        _mint(msg.sender, mintAllowance);
     }
 
 }
